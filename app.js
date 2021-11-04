d66c2a35c78120cd223362ee1267c69d3341e08d
Shahvez <shahvez01@gmail.com>
2021-11-04 21:22:34 +0100

    Adding functions

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+
+todoButton.addEventListener("click", addTodo);
+
+function addTodo(event) {
+  event.preventDefault();
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "hey";
+  newTodo.classList.add("todo-item");
+}
